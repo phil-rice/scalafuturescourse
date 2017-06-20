8198b31ec427ef18bba25f29a61fe941c2edfbed
Phil <phil.rice@iee.org>
2017-06-20 14:48:15 +0200

    Example answer to exercise 3

--- src/main/scala/Domain.scala
+++ src/main/scala/Domain.scala
@@ -1,10 +1,14 @@
 case class ArticleUrl(id: String)
 
-case class Article(content: String)
+case class Article(content: String) {
+  def withImages(images: Seq[CompressedImage]) = ArticleAndCompressedImages(this, images)
+}
 
 case class ImageUrl(id: String)
 
-case class ArticleAndImageUrls(article: Article, imageUrls: Seq[ImageUrl])
+case class ArticleAndImageUrls(article: Article, imageUrls: Seq[ImageUrl]){
+  def withImages(images: Seq[CompressedImage]) = ArticleAndCompressedImages(article, images)
+}
 
 case class ArticleAndCompressedImages(article: Article, images: Seq[CompressedImage])
 
--- src/main/scala/Services.scala
+++ src/main/scala/Services.scala
@@ -11,5 +11,32 @@ trait CompressionService extends Service[Image, CompressedImage]
 trait FullArticleService extends Service[ArticleUrl, ArticleAndCompressedImages]
 
 class FullArticleServiceImpl(implicit articleService: ArticleService, imageService: ImageService, compressionService: CompressionService, ec: ExecutionContext) extends FullArticleService {
-  override def apply(articleUrl: ArticleUrl): Future[ArticleAndCompressedImages] = ???
+  override def apply(articleUrl: ArticleUrl): Future[ArticleAndCompressedImages] = {
+    articleService(articleUrl).flatMap {
+      case (ArticleAndImageUrls(article, imageIds)) =>
+        Future.sequence(imageIds.map(imageId => imageService(imageId).flatMap(compressionService))).
+          map(article.withImages)
+    }
+  }
+}
+
+import Arrow._
+
+class FullArticleServiceImplWithArrows(implicit articleService: ArticleService, imageService: ImageService, compressionService: CompressionService, ec: ExecutionContext) extends FullArticleService {
+  override def apply(articleUrl: ArticleUrl) =
+    for {ArticleAndImageUrls(article, imageUrls) <- articleUrl ~> articleService
+         articleAndCompressedImages <- imageUrls ~> imageService >~> compressionService >>> article.withImages}
+      yield articleAndCompressedImages
+}
+
+class FullArticleServiceImplWithArrows2(implicit articleService: ArticleService, imageService: ImageService, compressionService: CompressionService, ec: ExecutionContext) extends FullArticleService {
+  override def apply(articleUrl: ArticleUrl) =
+    articleUrl ~> articleService >~> {
+      case ArticleAndImageUrls(article, imageUrls) => imageUrls ~> imageService >~> compressionService >>> article.withImages
+    }
+}
+
+class FullArticleServiceImplWithArrows3(implicit articleService: ArticleService, imageService: ImageService, compressionService: CompressionService, ec: ExecutionContext) extends FullArticleService {
+  override def apply(articleUrl: ArticleUrl) =
+    articleUrl ~> articleService >~> { articleAndImageIds => articleAndImageIds.imageUrls ~> imageService >~> compressionService >>> articleAndImageIds.withImages}
 }
